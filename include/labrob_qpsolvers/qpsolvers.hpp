91d185f54fbcfa527bfa0e8aa4ca7db0a7887ff1
Michele Cipriano <micco00x@gmail.com>
2021-12-06 08:57:53 +0100

    Dependency from qpOASES temporarily removed

--- include/labrob_qpsolvers/qpsolvers.hpp
+++ include/labrob_qpsolvers/qpsolvers.hpp
@@ -4,6 +4,6 @@
 
 // TODO: use CMake to include only available library
 #include "HPIPMQPSolver.hpp"
-#include "QPOASESQPSolver.hpp"
+//#include "QPOASESQPSolver.hpp"
 
 #include "QPSolverEigenWrapper.hpp"
